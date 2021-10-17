467d7c849623007e13a94aa5a8faf5f21243ab6f
givan991 <ivan.gnjidic@gmail.com>
2021-10-17 17:18:46 +0200

    Trial1

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -16,25 +16,25 @@ def main():
             print('Sets are NOT equal by subset test')
 
     print(
-    timeit.timeit(
-    stmt="expenses.categorize_for_loop",
-    setup='''
+    timeit.timeit(stmt="expenses.categorize_for_loop",
+    setup=
+    '''
 from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
-''',
+    ''',
     number=100000, globals=globals()
     )
     )
 
     print(
-    timeit.timeit(
-    stmt="expenses.categorize_set_comprehension",
-    setup='''
+    timeit.timeit(stmt="expenses.categorize_set_comprehension",
+    setup=
+    '''
 from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
-''',
+    ''',
     number=100000, globals=globals()
     )
     )
